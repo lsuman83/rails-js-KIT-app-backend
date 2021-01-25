03cde00482715d855ed5f5483b46ffd74f3584b8
Sean Bordelon <sean.j.bordelon@gmail.com>
2021-01-25 01:29:55 -0500

    seeding sample info into db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.first
+
+family_member1 = user.people.find_or_create_by(first_name: "Bob", last_name: "Dylan", relationship: "brother", birthday: "January 23, 2004",
+                            address: "1234 N. Hwy 200, Orlando, FL 34471", avatar_url: "https://tse4.mm.bing.net/th?id=OIP.GslTgQPuIwjafVpYBfGstAHaDt&pid=Api&P=0&w=368&h=185" )
+
+bobby_notes = user.notes.find_or_create_by(content: "we met", person_id: family_member1.id)
